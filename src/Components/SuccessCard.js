38982b30474da69f4c92a269a2e61178ec3051c4
Shailu2002 <shailusingh955960@gmail.com>
2025-10-26 14:32:30 +0530

    react-elastic-carousel removed added react-multi-caraousel

--- src/Components/SuccessCard.js
+++ src/Components/SuccessCard.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import Carousel from 'react-elastic-carousel';
+import  { useEffect, useState } from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 const SuccessCard = () => {
   const [data, setdata] = useState([]);
-  const Succesd = (element) =>
-  {
+  const Succesd = (element) => {
     return (
-      <div className="">
+      <div className="mb-5 mt-5">
         <div
           style={{ width: "300px", height: "70vh" }}
           className="card text-center"
@@ -32,67 +32,89 @@ const SuccessCard = () => {
         </div>
       </div>
     );
+  };
+
+  const getdata = async () => {
+    const res = await fetch("/getalldata", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const datas = await res.json();
+    console.log(datas);
+    if (!datas || res.status === 404) {
+    } else {
+      setdata(datas);
+      console.log(datas);
+      console.log("Data has been retrive");
     }
+  };
+  useEffect(() => {
+    getdata();
+  }, []);
 
-    const getdata = async () => {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1000 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1000, min:700 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max:700, min: 0 },
+      items: 1,
+    },
+  };
 
-
-        const res = await fetch('/getalldata', {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json"
-          }
-    
-        }
-        );
-        const datas = await res.json();
-        console.log(datas);
-        if (!datas|| res.status === 404) {
-        
-             
-        }
-        else {
-          
-          setdata(datas);
-          console.log(datas)
-          console.log("Data has been retrive");
-       
-         }
-      };
-      useEffect(() => {
-        getdata();
-      },[])
-    
-
-   
-    
-const breakPoints = [
-    { width: 1, itemsToShow: 1 },
-    { width: 550, itemsToShow: 1 },
-    { width: 768, itemsToShow: 2 },
-    { width: 1200, itemsToShow: 3 },
-    ];
-  
-  
   return (
-      <>
-       
-          <div className='bimg'>
-          <h1  style={{ textAlign: "center" ,color:"white", fontSize:"50px",fontFamily:"Kaushan Script",fontWeight:"bolder"}}>Success Stories....</h1>
-              <div style={{ paddingTop: "3%" }}>
-            
-                  <Carousel enableAutoPlay autoPlaySpeed={5000} breakPoints={breakPoints}>                          
-               {
-              data.map(Succesd)             
-             }    
-
-         
-          </Carousel>
-                  
+    <>
+      <div className="bimg">
+        <h1
+          style={{
+            textAlign: "center",
+            color: "white",
+            fontSize: "50px",
+            fontFamily: "Kaushan Script",
+            fontWeight: "bolder",
+          }}
+        >
+          Success Stories....
+        </h1>
+          <div>
+          {data.length > 0 ? (
+            <Carousel
+              infinite={true}
+              autoPlay={true}
+              showDots={true}
+              autoPlaySpeed={1000}
+              responsive={responsive}
+              ssr={true} // means to render carousel on server-side.
+              keyBoardControl={true}
+              removeArrowOnDeviceType={[
+                "tablet",
+                "mobile",
+                "desktop",
+                " superLargeDesktop",
+              ]}
+            >
+              {data.map(Succesd)}
+            </Carousel>
+          ) : (
+            <h4 style={{ textAlign: "center",marginTop:"15%" ,fontSize:"70px",color: "white" }}>
+              <i class="fa-solid fa-spinner fa-spin"></i>
+            </h4>
+          )}
+        </div>
       </div>
-      </div>  
-      </>
-  )
-}
+    </>
+  );
+};
 
 export default SuccessCard;
