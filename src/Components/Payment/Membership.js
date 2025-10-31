d4ca3c290fa3a10a2633cce5492c9a1573516b4d
Shailu2002 <shailusingh955960@gmail.com>
2025-11-01 00:03:54 +0530

    membership

--- src/Components/Payment/Membership.js
+++ src/Components/Payment/Membership.js
@@ -42,51 +42,78 @@ const Membership = () => {
     }
     
   return (
-      <>
-          <LoginNav />
-          
-       <div  className='backg text-center' >
-              <h4 style={{margin:"10px"}}   className=' heading1 text-danger'>Select Membership Plan</h4>
-          {
-              plan.map((element,id) =>
-              {
-                  return (
-                      <>
-                    <div className='plancon'>
-                <div className='container'>
-                    <div style={{margin:"15px"}} className='card cardp text-center' >
-                        <div className='card-body'>
-                             <p><span style={{ fontWeight: "bold" }}>{element.P_name}</span>  {element.P_duration } Months</p>
-                                 <p><span style={{ color: "green" }}> { element.P_discount}% off </span>
-                                 <span style={{ textDecoration: "line-through" }}> ₹ {element.P_amount} </span> </p>
-                                          <h2>₹{calculatediscount(element.P_discount,element.P_amount) }</h2>
-                                          <p>₹{ calculatedays(element.P_discount,element.P_amount,element.P_duration)} per month</p>
-                                          <button onClick={() =>
-                                          {
-                                            history('/qrpage', { state: {plan_name:element.P_name,duration:element.P_duration,amount:calculatediscount(element.P_discount,element.P_amount)} });
-                                    }} className='btn btnp btn-lg btn-outline-dark'>Continue</button>   
-                                          <p>
-                                           <ul>
-                                              {(element.P_description).split(",").map((element) =>
-                                              {
-                                                  return <li className='listp'>{element}</li>
-                                              })}
-                                     </ul>
-                                          </p>
-                                     </div>   
-                               
-                                      </div>
-                                  </div>
-                                 </div>
-                          </>)
-              })
-              }
-              </div>
-          
-                             
-        
-      </>
-  )
+    <>
+      <div className="backg text-center">
+        <h4 style={{ margin: "10px" }} className=" heading1 text-danger">
+          Select Membership Plan
+        </h4>
+        <div className="d-flex  flex-column flex-wrap  justify-content-center flex-sm-row ">
+          {plan.map((element, id) => {
+            return (
+              <>
+                <div className="card  m-3  border border-primary cardp text-center">
+                  <div className="card-body">
+                    <p>
+                      <span style={{ fontWeight: "bold" }}>
+                        {element.P_name}
+                      </span>{" "}
+                      {element.P_duration} Months
+                    </p>
+                    <p>
+                      <span style={{ color: "green" }}>
+                        {" "}
+                        {element.P_discount}% off{" "}
+                      </span>
+                      <span style={{ textDecoration: "line-through" }}>
+                        {" "}
+                        ₹ {element.P_amount}{" "}
+                      </span>{" "}
+                    </p>
+                    <h2>
+                      ₹{calculatediscount(element.P_discount, element.P_amount)}
+                    </h2>
+                    <p>
+                      ₹
+                      {calculatedays(
+                        element.P_discount,
+                        element.P_amount,
+                        element.P_duration
+                      )}{" "}
+                      per month
+                    </p>
+                    <button
+                      onClick={() => {
+                        history("/qrpage", {
+                          state: {
+                            plan_name: element.P_name,
+                            duration: element.P_duration,
+                            amount: calculatediscount(
+                              element.P_discount,
+                              element.P_amount
+                            ),
+                          },
+                        });
+                      }}
+                      className="btn btnp btn-lg btn-outline-dark"
+                    >
+                      Continue
+                    </button>
+                    <p>
+                      <ul>
+                        {element.P_description.split(",").map((element) => {
+                          return <li className="listp">{element}</li>;
+                        })}
+                      </ul>
+                    </p>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Membership;
